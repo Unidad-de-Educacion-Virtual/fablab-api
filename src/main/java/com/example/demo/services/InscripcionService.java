7b34f8cd2575774443aad0c0eb874670804a7dc1
Guerrerod14Ufps <127414316+Guerrerod14Ufps@users.noreply.github.com>
2025-03-02 10:43:42 -0500

    Update InscripcionService.java

--- src/main/java/com/example/demo/services/InscripcionService.java
+++ src/main/java/com/example/demo/services/InscripcionService.java
@@ -31,7 +31,7 @@ public class InscripcionService {
         Optional<Inscripcion> inscripcion = inscripcionRepository.findById(id);
         return inscripcion.get();
     }
-    @PreAuthorize("hasRole('ROLE_ADMIN', 'ROLE_INSTRUCTOR')")
+   @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_INSTRUCTOR')")
     public List<Inscripcion> listarInscripciones() {
         return inscripcionRepository.findAll();
     }
